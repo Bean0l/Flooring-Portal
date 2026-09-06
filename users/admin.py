36a7e7edf5a86ea1fa841c59e831c3ecd35ebfbc
llanosse-sys <llanosse@vcu.edu>
2026-09-06 11:12:27 -0400

    add custom user model with role and phone fields

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+
+# Controls how do the User model appears in the admin panel
+class CustomUserAdmin(UserAdmin):
+    # Controls how we view all users in a table
+    list_display = ('username', 'email', 'role', 'is_staff')
+    # Controls the detail view of a specific user when click to edit, also added another section called Role Info
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role Info', {'fields': ('role', 'phone')}),
+        )
+
+admin.site.register(User, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+# User DB table
+class User(AbstractUser):
+    # Types of roles a user can have
+    ROLES_CHOICES = (
+        ('admin', 'Admin'),
+        ('manager', 'Manager'),
+        ('employee', 'Employee'),
+    )
+
+    # Creates a role column in the DB table
+    role = models.CharField(max_length=20, choices=ROLES_CHOICES, default='employee')
+    # Creates a phone-number column in the DB table
+    phone = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+    
